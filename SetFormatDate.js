5c11221d39a8800aa744ca93b56c48a44d734dc7
Sopian <101189544+asopians@users.noreply.github.com>
2024-06-27 13:37:45 +0700

    Update SetFormatDate.js

--- SetFormatDate.js
+++ SetFormatDate.js
@@ -5,23 +5,14 @@
     PlusDate.setDate(today.getDate() + 30) // berdasarkan tgl
     PlusMonth.setMonth(today.getMonth() + 22) // berdasarkan bulan 
     PlusYear.setFullYear(today.getFullYear() + 10 ) //berdasarkan tahun
-    
-    //localdate
-    let thisDate = PlusMonth.toLocaleDateString('id-ID', {
-      day: 'numeric'
-    });
-    
-    //local Month Year
-    let thismonthyear = PlusMonth.toLocaleDateString('id-ID', {
-      month: 'long', year: 'numeric'
-    });
-    
-    //local Format
-    let allFormat = PlusMonth.toLocaleDateString('id-ID', {
-      day: 'numeric', month: 'long', year: 'numeric'
-    });
-    console.log(thisDate)
-    console.log(thismonthyear);
-    console.log(allFormat)
+
+    //local Format (hari, tgl, bulan dan tahun)
+    let allDate = PlusDate.toLocaleDateString('id-ID', { 
+        weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })
+
+    //format di balik
+    let ReverseDate = allDate.split("-").reverse().join("-");
+
+    console.log(allDate);
 
 ///if you wanna change local date(ex:id-ID) can see here https://www.w3schools.com/jsref/jsref_tolocalestring.asp#:~:text=%2C%20options)-,Parameter%20Values,-Parameter
